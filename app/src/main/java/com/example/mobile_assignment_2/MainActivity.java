c282ca5941105bc40c2ac7fa0f377ceb5c282239
Yicong Li <leo727268082@gmail.com>
2022-09-02 02:28:20 +1000

    added the navigationbar, which are home, chat and me

--- app/src/main/java/com/example/mobile_assignment_2/MainActivity.java
+++ app/src/main/java/com/example/mobile_assignment_2/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.mobile_assignment_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.mobile_assignment_2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.chat:
+                    replaceFragment(new ChatFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new MeFragment());
+                    break;
+            }
+
+            return true;
+
+        });
     }
+
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
+
+
+
 }
