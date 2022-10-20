e2497d4c6098aa8072820f5cdbb9df0a6eee918b
Yicong Li <leo727268082@gmail.com>
2022-10-20 16:23:44 +1100

    recycleview in chat

---
+++ app/src/main/java/com/example/mobile_assignment_2/message/MyViewAdapter.java
@@ -0,0 +1,20 @@
+package com.example.mobile_assignment_2.message;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewAdapter extends RecyclerView.ViewHolder {
+
+    public UserAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+}
