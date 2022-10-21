90a70e0bbe711cbf7287e284bf809633323a9df1
wentao-uw <wentao813@gmail.com>
2022-10-21 13:07:05 +1100

    add discover and community tab

--- app/src/main/java/com/example/mobile_assignment_2/community/CommunityFragment.java
+++ app/src/main/java/com/example/mobile_assignment_2/community/CommunityFragment.java
@@ -1,52 +1,32 @@
 package com.example.mobile_assignment_2.community;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.mobile_assignment_2.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CommunityFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CommunityFragment extends Fragment {
-    private ArrayList<Communitypost> posts = new ArrayList<>();
-    private RecyclerView recyclerView;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TabLayout tabLayout;
+    ViewPager2 viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     public CommunityFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CommunityFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CommunityFragment newInstance(String param1, String param2) {
         CommunityFragment fragment = new CommunityFragment();
         Bundle args = new Bundle();
@@ -63,104 +43,54 @@ public class CommunityFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        getActivity().setTitle("Foodies");
     }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.community_joinin_posts, container, false);
-//    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_community, container, false);
 
-        posts.add(new Communitypost(R.drawable.food, "commName1"));
-        posts.add(new Communitypost(R.drawable.food2, "commName2"));
-        posts.add(new Communitypost(R.drawable.food, "commName3"));
-        posts.add(new Communitypost(R.drawable.food2, "commName4"));
-
-        // Inflate the layout for this fragment
-
-        recyclerView =  view.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
-        recyclerView.setLayoutManager(gridLayoutManager);
-        CustomAdapter customAdapter = new CustomAdapter(posts);
-//        customAdapter.setClickListener(this);
-        recyclerView.setAdapter(customAdapter);
-
+        View view = inflater.inflate(R.layout.fragment_community_home,container,false);
+        tabLayout = view.findViewById(R.id.main_tab_layout);
+        viewPager = view.findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) ->  {
+                    if (position == 0) {
+                        tab.setText("Discover");
+                    } else {
+                        tab.setText("My Community");
+                    }
+                }
+        ).attach();
         return view;
     }
 
-    public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+    public class ViewPagerAdapter extends FragmentStateAdapter {
 
-        private ArrayList<Communitypost> posts = new ArrayList<Communitypost>();
-//        private PostItemClickListener postItemClickListener;
-        /**
-         * Provide a reference to the type of views that you are using
-         * (custom ViewHolder).
-         */
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            TextView titleView;
 
-            ImageView imgView;
+        public ViewPagerAdapter(Fragment fm) {
+            super(fm);
 
-            public ViewHolder(View view) {
-                super(view);
-                // Define click listener for the ViewHolder's View
-//                view.setOnClickListener(this);
-                titleView =   view.findViewById(R.id.communityName);
-                imgView =  view.findViewById(R.id.communityImg);
+        }
+
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new DiscoverCommunityFragment();
+//                case 1:
+//                    return new ForYouFragment();
+                default:
+                    return null;
             }
-
         }
 
-        /**
-         * Initialize the dataset of the Adapter.
-         *
-         * @param posts String[] containing the data to populate views to be used
-         * by RecyclerView.
-         */
-        public CustomAdapter(ArrayList<Communitypost> posts) {
-            this.posts = posts;
-
-        }
-//        public void setClickListener(PostItemClickListener postItemClickListener) {
-//            this.postItemClickListener = postItemClickListener;
-//        }
-        // Create new views (invoked by the layout manager)
-        @Override
-        public CustomAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-            // Create a new view, which defines the UI of the list item
-            View view = LayoutInflater.from(viewGroup.getContext())
-                    .inflate(R.layout.community_joinin_posts, viewGroup, false);
-
-            return new CustomAdapter.ViewHolder(view);
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(CustomAdapter.ViewHolder viewHolder, final int position) {
-
-            // Get element from your dataset at this position and replace the
-            // contents of the view with that element
-            //Post post = (Post) posts.get(position);
-            viewHolder.titleView.setText(posts.get(position).getCommName());
-
-            viewHolder.imgView.setBackgroundResource(posts.get(position).getCommImage());
-
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
         @Override
         public int getItemCount() {
-            return posts.size();
-        }
-        @Override
-        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-            super.onAttachedToRecyclerView(recyclerView);
+            return 2;
         }
     }
 }
