9d046225dc43ccf4bc8e293e958d907d872b3778
wentao-uw <wentao813@gmail.com>
2022-10-24 10:09:57 +1100

    add delete function in my communitiy

--- app/src/main/java/com/example/mobile_assignment_2/community/DiscoverCommunityFragment.java
+++ app/src/main/java/com/example/mobile_assignment_2/community/DiscoverCommunityFragment.java
@@ -65,12 +65,6 @@ public class DiscoverCommunityFragment extends Fragment {
         }
     }
 
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.community_joinin_posts, container, false);
-//    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/example/mobile_assignment_2/community/MyCommunityFragment.java
+++ app/src/main/java/com/example/mobile_assignment_2/community/MyCommunityFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -56,42 +58,96 @@ public class MyCommunityFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_my_community, container, false);
 
 
-        ArrayList<Communitypost> posts = new ArrayList<>();
-
+        List<Communitypost> posts = new ArrayList<>();
+        getcommPosts(posts);
         CustomAdapter customAdapter = new CustomAdapter(getContext(), posts);
         listView = view.findViewById(R.id.list);
         listView.setAdapter(customAdapter);
-        ArrayList<Communitypost> newPosts = new ArrayList<>();
-        newPosts.add(new Communitypost(R.drawable.food, "commName1"));
-        newPosts.add(new Communitypost(R.drawable.food2, "commName2"));
-        newPosts.add(new Communitypost(R.drawable.food, "commName3"));
-        newPosts.add(new Communitypost(R.drawable.food2, "commName4"));
-        customAdapter.addAll(newPosts);
+//        customAdapter.addAll(getcommPosts());
 
         return view;
     }
 
-    public class CustomAdapter extends ArrayAdapter<Communitypost> {
+    public class CustomAdapter extends BaseAdapter {
+        private List<Communitypost> musers = null;
+        private Context mcontext = null;
 
-        public CustomAdapter(Context context, ArrayList<Communitypost> users) {
-            super(context, 0, users);
+
+        public CustomAdapter(Context context, List<Communitypost> users) {
+            musers = users;
+            mcontext = context;
+//            super(context, 0, users);
         }
+
+        @Override
+        public int getCount() {
+            return musers.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return musers.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public final class ViewHolder {
+            ImageView image;
+            TextView title;
+            TextView content;
+            Button delete;
+        }
+
 
         public View getView(int position, View convertView, ViewGroup parent) {
-            Communitypost post = getItem(position);
+            ViewHolder holder;
+//            Communitypost post = getItem(position);
             if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.community_leave_posts, parent, false);
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(mcontext).inflate(R.layout.community_leave_posts, null);
+                holder.image = convertView.findViewById(R.id.communityImg);
+                holder.title = convertView.findViewById(R.id.community_name);
+                holder.delete = convertView.findViewById(R.id.btn_leave);
+                holder.delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        musers.remove(position);
+                        notifyDataSetChanged();
+
+                    }
+                });
+                convertView.setTag(holder);
+            }else {
+                holder = (ViewHolder) convertView.getTag();
             }
 
-            TextView titleView =  (TextView) convertView.findViewById(R.id.community_name);
+//            TextView titleView =  (TextView) convertView.findViewById(R.id.community_name);
 //            TextView descpView =  (TextView) convertView.findViewById(R.id.community_description);
-            ImageView commImg =  convertView.findViewById(R.id.communityImg);
+//            ImageView commImg =  convertView.findViewById(R.id.communityImg);
+//            Button delBtn = convertView.findViewById(R.id.btn_leave);
 
-            titleView.setText(post.getCommName());
+
+
+//            titleView.setText(post.getCommName());
 //            descpView.setText(post.getDescription());
-            commImg.setBackgroundResource(post.getCommImage());
+//            commImg.setBackgroundResource(post.getCommImage());
+            holder.image.setBackgroundResource(musers.get(position).getCommImage());
+            holder.title.setText(musers.get(position).getCommName());
             return  convertView;
         }
+    }
+
+    // To generate an array of posts example
+    private List<Communitypost> getcommPosts(List<Communitypost> newPosts) {
+//        ArrayList<Communitypost> newPosts = new ArrayList<>();
+        newPosts.add(new Communitypost(R.drawable.food, "commName1"));
+        newPosts.add(new Communitypost(R.drawable.food2, "commName2"));
+        newPosts.add(new Communitypost(R.drawable.food, "commName3"));
+        newPosts.add(new Communitypost(R.drawable.food2, "commName4"));
+        return newPosts;
     }
 
 
