a4b6e6f44c6be14c01cb758f6f8845d01fd80a60
Weiran Zou <zouweiran9122@gmail.com>
2022-10-25 19:53:03 +1100

    Update Home's 'for you' tab: Get posts from firebase and display posts of user's friends and user

--- app/src/main/java/com/example/mobile_assignment_2/home/ForYouFragment.java
+++ app/src/main/java/com/example/mobile_assignment_2/home/ForYouFragment.java
@@ -2,12 +2,16 @@ package com.example.mobile_assignment_2.home;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +22,13 @@ import android.widget.TextView;
 
 import com.example.mobile_assignment_2.Post;
 import com.example.mobile_assignment_2.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -38,8 +49,12 @@ public class ForYouFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    ArrayList<Post> posts = new ArrayList<>();
     CustomAdapter customAdapter;
     RecyclerView recyclerView;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+
     public ForYouFragment() {
         // Required empty public constructor
     }
@@ -76,19 +91,61 @@ public class ForYouFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_for_you, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        ArrayList<String> friends = new ArrayList<>();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        // Get a reference to users
+        DatabaseReference usersRef = firebaseDatabase.getReference("Users");
+        usersRef.child(currentUser.getUid()).child("friends").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String friendId = dataSnapshot.getKey();
+                    friends.add(friendId);
+                }
+                DatabaseReference postsRef = firebaseDatabase.getReference("Posts");
+                postsRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Post post = dataSnapshot.getValue(Post.class);
+                            posts.add(post);
+                        }
+                        // posts for user and user's friends
+                        ArrayList<Post> forYouPosts = new ArrayList<>();
+                        for (Post p : posts) {
+                            if (friends.contains(p.getUid()) || p.getUid().equals(currentUser.getUid())) {
+                                forYouPosts.add(p);
+                            }
+                        }
+                        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+                        recyclerView.setHasFixedSize(true);
+                        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        customAdapter = new CustomAdapter(forYouPosts);
+                        recyclerView.setAdapter(customAdapter);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
 
 
-        ArrayList<Post> forYouPosts = new ArrayList<>();
-        forYouPosts.add(new Post("1","a","","",new ArrayList<>()));
-        forYouPosts.add(new Post("2","b","","",new ArrayList<>()));
-        forYouPosts.add(new Post("3","c","","",new ArrayList<>()));
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        customAdapter = new CustomAdapter(forYouPosts);
-        recyclerView.setAdapter(customAdapter);
+        });
         return view;
+
     }
 
     public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
@@ -132,6 +189,11 @@ public class ForYouFragment extends Fragment {
             viewHolder.titleView.setText(posts.get(position).getTitle());
             viewHolder.authorView.setText(posts.get(position).getAuthor());
             viewHolder.descpView.setText(posts.get(position).getDescription());
+            String imageUrl = posts.get(position).getImageUrl().get(0);
+
+            // Download image from URL and set to imageView
+            //Picasso.with(getContext()).load(imageUrl).into(viewHolder.imageView);
+            Picasso.with(getContext()).load("https://cdn.concreteplayground.com/content/uploads/2017/08/Glamp-Cocktail-Bar-32-1920x1440.jpg").into(viewHolder.imageView);
 
         }
 
