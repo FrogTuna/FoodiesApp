ea963d4c989edcee77b437a4fb3fb695157f2b14
KeYANG813 <yk1148910416@163.com>
2022-10-26 16:27:54 +1100

    Ke Yang

---
+++ app/src/main/java/com/example/mobile_assignment_2/add/CustomItem.java
@@ -0,0 +1,28 @@
+package com.example.mobile_assignment_2.add;
+
+public class CustomItem {
+
+    private String spinnerItemName;
+    private int spinnerItemImage;
+
+    public CustomItem(String spinnerItemName, int spinnerItemImage){
+        this.spinnerItemName=spinnerItemName;
+        this.spinnerItemImage=spinnerItemImage;
+    }
+
+    public void setSpinnerItemImage(int spinnerItemImage) {
+        this.spinnerItemImage = spinnerItemImage;
+    }
+
+    public void setSpinnerItemName(String spinnerItemName) {
+        this.spinnerItemName = spinnerItemName;
+    }
+
+    public int getSpinnerItemImage() {
+        return spinnerItemImage;
+    }
+
+    public String getSpinnerItemName() {
+        return spinnerItemName;
+    }
+}
