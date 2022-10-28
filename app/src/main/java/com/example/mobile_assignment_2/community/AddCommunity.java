0de2b4e3ed1673263cdcdd1637e27e106e500ada
Yujia Zhu <yujzhu1@student.unimelb.edu.au>
2022-10-28 18:34:17 +1100

    data community change

--- app/src/main/java/com/example/mobile_assignment_2/community/AddCommunity.java
+++ app/src/main/java/com/example/mobile_assignment_2/community/AddCommunity.java
@@ -2,8 +2,14 @@ package com.example.mobile_assignment_2.community;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobile_assignment_2.R;
@@ -11,16 +17,28 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
-public class AddCommunity extends AppCompatActivity {
+public class AddCommunity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
     private TextInputEditText comNameView;
     private TextInputEditText comDescriptionView;
+    private Spinner comType;
+    String[] comType_list = { "Asian food", "American food", "Italy food", "Greece food", "Mexican food"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_create_community);
+        comType = findViewById(R.id.com_type);
+        comType.setOnItemSelectedListener(this);
+
+        // Create the instance of ArrayAdapter
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, comType_list);
+
+        // set simple layout resource file for each item of spinner
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the Spinner which binds data to spinner
+        comType.setAdapter(ad);
+
         Intent intent = getIntent();
-
-
         comNameView = findViewById(R.id.community_name);
         comDescriptionView = findViewById(R.id.com_desc);
         String name = comNameView.getText().toString();
@@ -30,4 +48,15 @@ public class AddCommunity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),
+                        comType_list[position],
+                        Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
